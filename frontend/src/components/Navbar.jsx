85c0468065a86c8168ca3338ab1eb2dd3eda54a5
PRATIK5311 <144891520+PRATIK5311@users.noreply.github.com>
2025-04-15 13:57:49 +0530

    first5

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -41,8 +41,10 @@ const Navbar = () => {
       </ul>
 
       <div className='flex items-center gap-4 '>
+        {console.log(token, userData)}
+        
         {
-          token && userData
+          token // userData
             ? <div className='flex items-center gap-2 cursor-pointer group relative'>
               <img className='w-8 rounded-full' src={userData.image} alt="" />
               <img className='w-2.5' src={assets.dropdown_icon} alt="" />
